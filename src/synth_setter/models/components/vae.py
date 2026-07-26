31008b23949a0276ae1b94cefba33673e20b2c02
KT <17952332+ktinubu@users.noreply.github.com>
2026-07-26 11:26:24 -0400

    internal-fix(training): defer param loss slice routing

--- src/synth_setter/models/components/vae.py
+++ src/synth_setter/models/components/vae.py
@@ -356,17 +356,38 @@ def compute_individual_parameter_loss(
 
 
 def param_loss(x_hat: torch.Tensor, x: torch.Tensor, param_spec: str) -> torch.Tensor:
-    spec = param_specs[param_spec]
+    param_spec = param_specs[param_spec]
 
-    assert spec.encoded_width == x.shape[1]
+    synth_params = [(p, len(p)) for p in param_spec.synth_params]
+    note_params = [(p, len(p)) for p in param_spec.note_params]
 
-    spans = list(spec.encoded_slices())
-    loss = sum(
-        compute_individual_parameter_loss(x_hat[:, span], x[:, span], param)
-        for param, span in spans
-    )
+    pointer = 0
 
-    return loss / len(spans)
+    loss = 0.0
+
+    for param, length in synth_params:
+        x_param = x[:, pointer : pointer + length]
+        x_hat_param = x_hat[:, pointer : pointer + length]
+
+        this_loss = compute_individual_parameter_loss(x_hat_param, x_param, param)
+
+        loss += this_loss
+
+        pointer += length
+
+    for param, length in note_params:
+        x_param = x[:, pointer : pointer + length]
+        x_hat_param = x_hat[:, pointer : pointer + length]
+
+        this_loss = compute_individual_parameter_loss(x_hat_param, x_param, param)
+
+        loss += this_loss
+
+        pointer += length
+
+    assert pointer == x.shape[1]
+
+    return loss / (len(synth_params) + len(note_params))
 
 
 def compute_flowvae_loss(
