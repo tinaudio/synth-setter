07bd1e8279d32e3ef8db5902ba5dafbaf7d036d8
KT <17952332+ktinubu@users.noreply.github.com>
2026-09-10 14:24:48 -0400

    internal-feat(evaluation): log semantic parameter distances (#3468)

--- tests/tools/test_wandb_parameter_workspace.py
+++ tests/tools/test_wandb_parameter_workspace.py
@@ -42,6 +42,10 @@ def test_parameter_workspace_serializes_dynamic_metric_regexes() -> None:
         "^val/per_param_mse_spec_quantized/.+$",
         "^val/categorical_mismatch_rate/.+$",
         "^val/number_group_optimal_assignment_categorical_mismatch_rate/.+$",
+        "^val/angular_mae_radians/.+$",
+        "^val/axis_angular_error_radians/.+$",
+        "^val/discrete_(mae|mismatch_rate)/.+$",
+        "^val/note_timing_mae_seconds/.+$",
     ]
     assert all(config["useMetricRegex"] is True for config in panel_configs)
     assert spec["runSets"][0]["search"]["query"] == ""
